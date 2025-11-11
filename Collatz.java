94eb7deed25da5f684d74438c3f730dca68b1559
talkz199 <tal.katz@runi.post.ac.il>
2025-11-11 03:00:08 +0200

    finish collatz

--- Collatz.java
+++ Collatz.java
@@ -2,5 +2,63 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int N = Integer.parseInt(args[0]);
+		char mode = args[1].charAt(0);
+		
+		
+
+		
+			for (int i=1; i <=N;i++){
+				int seed = i;
+				int count = 1;
+				if (mode == 'v'){
+			
+					System.out.print(seed);
+				}
+				
+				if (seed % 2 ==0 ){
+				
+					seed = seed/2 ;
+					if (mode == 'v'){
+					System.out.print(" " + seed);
+					}
+					count++;
+				}else {
+					seed = (seed * 3 + 1);
+					if (mode == 'v'){
+					System.out.print( " " +seed);
+					}
+					count++;
+					
+				}
+
+				while (seed>1){
+					if (seed % 2 ==0 ){
+				
+						seed = seed/2 ;
+						if (mode == 'v'){
+						System.out.print(" " + seed);
+						}
+						count++;
+					}else {
+						seed = (seed * 3 + 1);
+						if (mode == 'v'){
+						System.out.print( " " +seed);
+						}
+						count++;
+						
+						}		
+					}
+					if (mode == 'v'){
+					System.out.println(" (" + count + ")");
+					System.out.println();
+					}
+					
+				}
+				System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+			
+			
+		}
 	}
-}
+
+	
